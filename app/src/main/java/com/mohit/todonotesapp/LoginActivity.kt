85fabbe3d5636d2e5bbe028c40beea135831ff7f
Mohit Goel <itsmohitgoel@gmail.com>
2020-05-02 08:12:18 +0530

    Add LoginActivity with two edit textviews and login button

---
+++ app/src/main/java/com/mohit/todonotesapp/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.mohit.todonotesapp
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class LoginActivity : AppCompatActivity() {
+
+    @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
