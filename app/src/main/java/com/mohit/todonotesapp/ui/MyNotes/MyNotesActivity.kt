b87702c8c0303460d6a01ce480076bacde195ee1
Mohit Goel <itsmohitgoel@gmail.com>
2020-05-09 07:32:18 +0530

    Pass data from LoginActivity to MyNotesActivity via Intent

--- app/src/main/java/com/mohit/todonotesapp/ui/MyNotes/MyNotesActivity.kt
+++ app/src/main/java/com/mohit/todonotesapp/ui/MyNotes/MyNotesActivity.kt
@@ -1,6 +1,8 @@
 package com.mohit.todonotesapp.ui.MyNotes
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.mohit.todonotesapp.R
 
@@ -8,5 +10,8 @@ class MyNotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_notes)
+
+        val intent = this.intent
+        Log.d("IntentDataPass", intent.getStringExtra("full_name"))
     }
 }
--- app/src/main/java/com/mohit/todonotesapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mohit/todonotesapp/ui/login/LoginActivity.kt
@@ -1,14 +1,41 @@
 package com.mohit.todonotesapp.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.mohit.todonotesapp.R
+import com.mohit.todonotesapp.ui.MyNotes.MyNotesActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var editTextFullName : EditText
+    private lateinit var editTextUserName : EditText
+    private lateinit var buttonLogin : Button
 
     @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        editTextFullName = etFullName
+        editTextUserName = etFullName
+        this.buttonLogin = btnLogin
+
+        this.buttonLogin.setOnClickListener {
+            Log.d("Click", "onClick performed")
+            val fullName = editTextFullName.text.toString()
+            val intent = Intent(this@LoginActivity, MyNotesActivity::class.java).apply {
+                putExtra("full_name", fullName)
+            }
+            startActivity(intent)
+        }
     }
 }
