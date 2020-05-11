f07c10be8e49c0e9d3bbfa8da2f689b00f95a195
Mohit Goel <itsmohitgoel@gmail.com>
2020-05-11 18:07:18 +0530

    Add dialog box for title and description of note using LayoutInflater and AlertDialog.
    Also adds floating_action_button for popping up Dialog box.

--- app/src/main/java/com/mohit/todonotesapp/ui/MyNotes/MyNotesActivity.kt
+++ app/src/main/java/com/mohit/todonotesapp/ui/MyNotes/MyNotesActivity.kt
@@ -3,17 +3,60 @@ package com.mohit.todonotesapp.ui.MyNotes
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mohit.todonotesapp.R
+import kotlinx.android.synthetic.main.activity_my_notes.*
+import kotlinx.android.synthetic.main.layout_dialog_add_my_notes.*
+import kotlinx.android.synthetic.main.layout_dialog_add_my_notes.view.*
 
 class MyNotesActivity : AppCompatActivity() {
+
+    private lateinit var textViewMyNotesTitle: TextView
+    private lateinit var textViewMyNotesDescription: TextView
+    private lateinit var floatingButtonAddNotes: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_notes)
+
+        textViewMyNotesTitle = tvMyNotesTitle
+        textViewMyNotesDescription = tvMyNotesDescription
+        floatingButtonAddNotes = fabAddNotes
+
+        floatingButtonAddNotes.setOnClickListener {
+            setUpDialog()
+        }
 
         val intent = this.intent
         val fullName = intent.getStringExtra("full_name")
         supportActionBar?.title = fullName
         Log.d("IntentDataPass", intent.getStringExtra("full_name"))
+    }
+
+    private fun setUpDialog() {
+        val view = LayoutInflater.from(this@MyNotesActivity)
+            .inflate(R.layout.layout_dialog_add_my_notes, null)
+
+        val editTextTitle = view.etDialogTitle
+        val editTextDesciption = view.etDialogDesciption
+        val buttonSubmit = view.btnSubmitDialog
+
+        val alertDialog : AlertDialog = AlertDialog.Builder(this@MyNotesActivity)
+            .setView(view).setCancelable(false).create()
+
+        buttonSubmit.setOnClickListener {
+            textViewMyNotesTitle.text = editTextTitle.text.toString()
+            textViewMyNotesDescription.text = editTextDesciption.text.toString()
+
+            alertDialog.hide()
+
+        }
+
+        alertDialog.show()
+
     }
 }
