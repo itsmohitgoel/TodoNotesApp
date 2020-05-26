1019481e8c627244fd70da0035046fa2fedf9208
Mohit Goel <itsmohitgoel@gmail.com>
2020-05-26 10:55:18 +0530

    Define function in Application class, to provide Database instance.

--- app/src/main/java/com/mohit/todonotesapp/NotesApp.kt
+++ app/src/main/java/com/mohit/todonotesapp/NotesApp.kt
@@ -1,9 +1,13 @@
 package com.mohit.todonotesapp
 
 import android.app.Application
+import com.mohit.todonotesapp.data.local.db.NotesDatabase
 
 class NotesApp : Application() {
     override fun onCreate() {
         super.onCreate()
     }
+
+    fun getNotesDb(): NotesDatabase =
+        NotesDatabase.getInstance(this)
 }
