3989de88f9f3d93db71070ffa3a48e26c2883e12
Mohit Goel <itsmohitgoel@gmail.com>
2020-05-28 19:14:18 +0530

    Add `AddNotesActivity` with respective layout.
    Going forward this will be used to add new notes to our app, instead
    of present available dialog box, drawn on top of MyNotesActivity.

---
+++ app/src/main/java/com/mohit/todonotesapp/ui/addNotes/AddNotesActivity.kt
@@ -0,0 +1,13 @@
+package com.mohit.todonotesapp.ui.addNotes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mohit.todonotesapp.R
+
+class AddNotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notes)
+    }
+}
