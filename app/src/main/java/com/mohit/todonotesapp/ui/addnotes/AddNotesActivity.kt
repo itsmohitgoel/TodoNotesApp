27abb5a67d0ed22bc327d747ed3b5540508b3ed5
Mohit Goel <itsmohitgoel@gmail.com>
2020-05-30 07:59:18 +0530

    Implement view binds in AddNotesActivity and use implicit intent to browse image for note representation.

--- app/src/main/java/com/mohit/todonotesapp/ui/addnotes/AddNotesActivity.kt
+++ app/src/main/java/com/mohit/todonotesapp/ui/addnotes/AddNotesActivity.kt
@@ -1,11 +1,24 @@
 package com.mohit.todonotesapp.ui.addnotes
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 import com.mohit.todonotesapp.R
-import kotlinx.android.synthetic.main.activity_add_notes.*
+import kotlinx.android.synthetic.main.layout_dialog_selector.view.*
 
 class AddNotesActivity : AppCompatActivity() {
+
+    lateinit var etNoteTitle: EditText
+    lateinit var etNoteDescription: EditText
+    lateinit var btnSubmit: MaterialButton
+    lateinit var ivAddNotes: ImageView
+    val REQUEST_CODE_GALLERY = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,20 +26,40 @@ class AddNotesActivity : AppCompatActivity() {
 
         bindViews()
         setUpClickListeners()
+    }
+
+    private fun bindViews() {
+        etNoteTitle = findViewById(R.id.etNoteTitle)
+        etNoteDescription = findViewById(R.id.etNoteDescription)
+        btnSubmit = findViewById(R.id.btnSubmit)
+        ivAddNotes = findViewById(R.id.ivAddNoteImage)
+    }
+
+    private fun setUpClickListeners() {
         ivAddNotes.setOnClickListener {
             setupDialog()
         }
     }
 
-    private fun bindViews() {
-        TODO("Not Implemented Yet")
-    }
-
-    private fun setUpClickListeners() {
-        TODO("Not Implemented Yet")
-    }
-
     private fun setupDialog() {
-        TODO("Not Implemented Yet")
+        val view = LayoutInflater.from(this)
+            .inflate(R.layout.layout_dialog_selector, null)
+
+        val tvDialogCamera = view.tvDialogCamera
+        val tvDialogGallery = view.tvDialogGallery
+
+        val dialog: AlertDialog = AlertDialog.Builder(this)
+            .setView(view).setCancelable(false).create()
+
+        tvDialogCamera.setOnClickListener {
+            TODO("NOT YET Implemented")
+        }
+
+        tvDialogGallery.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_CODE_GALLERY)
+        }
+
+        dialog.show()
     }
 }
