de22dc953a0bb0c09de8067d5db859023894cb71
Mohit Goel <itsmohitgoel@gmail.com>
2020-06-04 11:35:18 +0530

    Request permissions using request-code and handle a denial as well.
    Based on user's consent it will either capture images from Gallery or
    won't.

--- app/src/main/java/com/mohit/todonotesapp/ui/addnotes/AddNotesActivity.kt
+++ app/src/main/java/com/mohit/todonotesapp/ui/addnotes/AddNotesActivity.kt
@@ -1,13 +1,19 @@
 package com.mohit.todonotesapp.ui.addnotes
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.google.android.material.button.MaterialButton
 import com.mohit.todonotesapp.R
@@ -21,6 +27,7 @@ class AddNotesActivity : AppCompatActivity() {
     lateinit var ivAddNotes: ImageView
     val REQUEST_CODE_GALLERY = 2
     val REQUEST_CODE_CAMERA = 1
+    val REQUEST_CODE_PERMISSION = 124
     var imagePath = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +47,8 @@ class AddNotesActivity : AppCompatActivity() {
 
     private fun setUpClickListeners() {
         ivAddNotes.setOnClickListener {
-            setupDialog()
+            if (checkAndRequestPermission())
+                setupDialog()
         }
     }
 
@@ -51,7 +59,7 @@ class AddNotesActivity : AppCompatActivity() {
         val tvDialogCamera = view.tvDialogCamera
         val tvDialogGallery = view.tvDialogGallery
 
-        val dialog: AlertDialog = AlertDialog.Builder(this)
+        val permissionDialog: AlertDialog = AlertDialog.Builder(this)
             .setView(view).setCancelable(false).create()
 
         tvDialogCamera.setOnClickListener {
@@ -61,29 +69,72 @@ class AddNotesActivity : AppCompatActivity() {
         tvDialogGallery.setOnClickListener {
             val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             startActivityForResult(intent, REQUEST_CODE_GALLERY)
+
+            permissionDialog.hide()
         }
 
-        dialog.show()
+        permissionDialog.show()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        when (requestCode) {
-            REQUEST_CODE_GALLERY -> {
-                val selectedImage = data?.data
-                val filePath = arrayOf(MediaStore.Images.Media.DATA)
-                val contentResolver = contentResolver.query(
-                    selectedImage!!, filePath, null, null, null
-                )
-                contentResolver!!.moveToFirst()
-                val columnIndex = contentResolver.getColumnIndex(filePath[0])
-                imagePath = contentResolver.getString(columnIndex)
-                contentResolver.close()
-                Glide.with(this).load(imagePath).into(ivAddNotes)
-            }
-            REQUEST_CODE_CAMERA -> {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_CODE_GALLERY -> {
+                    val selectedImage = data?.data
+                    val filePath = arrayOf(MediaStore.Images.Media.DATA)
+                    val contentResolver = contentResolver.query(
+                        selectedImage!!, filePath, null, null, null
+                    )
+                    contentResolver!!.moveToFirst()
+                    val columnIndex = contentResolver.getColumnIndex(filePath[0])
+                    imagePath = contentResolver.getString(columnIndex)
+                    contentResolver.close()
+                    Glide.with(this).load(imagePath).into(ivAddNotes)
+                }
+                REQUEST_CODE_CAMERA -> {
 
+                }
+            }
+        }
+    }
+
+    private fun checkAndRequestPermission(): Boolean {
+        val cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        val externalStorage = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        val listPermissionNeeded = ArrayList<String>()
+
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionNeeded.add(Manifest.permission.CAMERA)
+        }
+
+        if (externalStorage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+
+        if (listPermissionNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionNeeded.toTypedArray(), REQUEST_CODE_PERMISSION)
+            return false
+        }
+
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            REQUEST_CODE_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    setupDialog()
+                else {
+                    Toast.makeText(
+                        this@AddNotesActivity, "Permission is needed to browse image or click new image",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         }
     }
